9b00cdce047b04e603e7f34d719768808c58bdcf
Jonathan van Alteren <jvalteren@essentialimagination.nl>
2018-08-12 15:17:40 +0200

    Added token required option and max price optional option. Fixed user agent header. Added referer header and changed browser to open to Firefox Developer Edition.

--- bin/command.js
+++ bin/command.js
@@ -13,12 +13,23 @@ const argv = require('yargs')
         required: true,
         describe: 'Session ID retrieved from your ticketswap.nl cookie',
     })
+    .option('token', {
+        alias: 't',
+        required: true,
+        describe: 'Token retrieved from your ticketswap.nl cookie',
+    })
     .option('amount', {
         alias: 'n',
         default: 1,
         describe: 'The amount of tickets to reserve',
     })
+    .option('max_price', {
+        alias: 'p',
+        default: 0,
+        describe: 'The maximum price you want to pay',
+    })
     .demandOption('s', 'We need your session id to reserve tickets')
+    .demandOption('t', 'To ensure not getting kicked out, we need your token')
     .demandCommand(2)
     .help()
     .argv;
@@ -26,8 +37,10 @@ const argv = require('yargs')
 const options = { 
     url: argv._[1],
     baseUrl: 'https://www.ticketswap.nl',
-    amount: argv['n'],
     sessionID: argv['s'],
+    token: argv['t'],
+    amount: argv['n'],
+    max_price: argv['p']
 };
 
 function mask(input) {
@@ -38,8 +51,10 @@ function mask(input) {
 logger.info(`${chalk.green('TicketScoop')} now running with configuration:`);
 logger.info([
     ` ${chalk.magenta('url')}       = ${options.url}`,
-    ` ${chalk.magenta('amount')}    = ${options.amount}`,
     ` ${chalk.magenta('sessionID')} = ${mask(options.sessionID)}`,
+    ` ${chalk.magenta('token')}     = ${options.token}`,
+    ` ${chalk.magenta('amount')}    = ${options.amount}`,
+    ` ${chalk.magenta('max_price')} = ${options.max_price}`,
     '',
 ].join('\n'))
 
